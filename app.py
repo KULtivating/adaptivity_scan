4a178f92332c365c257c5559ba608f48c0441cc7
KULtivating <jana.deprez@gmail.com>
2026-04-30 10:24:53 +0200

    app.py

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def connect_sheet():
         "https://www.googleapis.com/auth/drive"
     ]
 
-    creds = Credentials.from_service_account_file(
+    creds = Credentials.from_service_account_info(
         st.secrets["gcp_service_account"],
         scopes=scope
     )
